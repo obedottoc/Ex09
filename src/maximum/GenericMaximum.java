b71fc8dff2f40d19c264b147302bb637c7ef7eca
user3 <user3@administrator-HP-Pro-3330-MT>
2019-09-21 08:22:39 +0530

    generic maximum

--- src/maximum/GenericMaximum.java
+++ src/maximum/GenericMaximum.java
@@ -1,5 +1,8 @@
-package maximum;
-public class  GenericMaximum{
+/*generic concept
+ * developed by 
+ * suriya kumar
+ * 212217105057*/package maximum;
+public class GenericMaximum {
 
 	public static <E extends Comparable<E>>E Max (E[] elements) {
 		E m;
@@ -14,11 +17,11 @@ public class  GenericMaximum{
 }
 
 public static void main(String[] arg) {
-	 Integer[] intArray = {12,78,7,67,32};
+	 Integer[] intArray = {2,7,6,21,40};
 	 Integer intMax;
-     Double[] doubleArray = { 12.0, 23.5, 34.0, 4.5, 7.0 };
+     Double[] doubleArray = { 1.0,50.0,100.5,20.4,46.21};
      Double doubleMax;
-     String[] strArray= {"aa","bb","cc","dd","ee"};
+     String[] strArray= {"suri","vox","problem","eee","subject"};
      String strMax;
      intMax=GenericMaximum.Max(intArray);
      System.out.println("Intger Max="+intMax);
